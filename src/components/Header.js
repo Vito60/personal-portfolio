65654950c290278404feae0b69e68da42ba88da8
Vito60 <randallw1996@gmail.com>
2020-09-10 17:48:42 -0400

    created box component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+    Typography,
+    Avatar,
+    Grid,
+    Box
+} from '@material-ui/core'
+
+export default function Header() {
+    return (
+        <Box>
+            
+        </Box>
+    )
+}
