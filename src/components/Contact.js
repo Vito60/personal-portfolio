b5fa9f82e710cbbf7df2edcc0599f3bb67e9740f
Vito60 <randallw1996@gmail.com>
2020-09-18 14:02:03 -0400

    changed name of env variables

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -49,9 +49,9 @@ export default function Contact() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [content, setContent] = useState("");
-    const templateId = '{process.env.NODE_ENV_TEMPLATE_ID}'; 
-    const serviceId = '{process.env.NODE_ENV_SERVICE_ID}';
-    const userId = '{process.env.NODE_ENV_USER_ID}';
+    const templateId = '{process.env.REACT_APP_TEMPLATE_ID}'; 
+    const serviceId = '{process.env.REACT_APP_SERVICE_ID}';
+    const userId = '{process.env.REACT_APP_USER_ID}';
 
     const handleSubmit = (contentValue, nameValue, emailValue) => {
         sendEmail({message: contentValue, from_name: nameValue, reply_to: emailValue})
